c58919f5f9719dabf2eb5be50a68a095b6ca7851
Ben Westover <kwestover.kw@gmail.com>
2021-08-22 10:46:40 -0400

    forgot config

---
+++ config/bot.js
@@ -0,0 +1,20 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'GET',
+        prefix: '!',
+        watching: 'TSC',
+    },
+
+
+
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
