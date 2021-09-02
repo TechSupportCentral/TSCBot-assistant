14a1e3430f21eb01693ce41a2d0fc9f798dbd7a0
Philipp-Spec <subwayphil801@gmail.com>
2021-09-02 19:16:16 +0200

    Update Events

--- events/message.js
+++ events/message.js
@@ -1,7 +1,7 @@
 const customschema = require('../schemas/custom-commands');
 
 module.exports = (client, message) => {
-    async function yup() {
+    
      if (message.author.bot || message.channel.type === 'dm') return;
     const prefix = client.config.discord.prefix;
     if (message.content.indexOf(prefix) !== 0) return;
@@ -10,6 +10,6 @@ module.exports = (client, message) => {
     const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
     if (cmd) cmd.execute(client, message, args)
     
-}   
-    yup()
+
+  
 };
