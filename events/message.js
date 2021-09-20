4a15fab1de4530cd1efcec31634e75dbf8b59d70
Philipp-Spec <subwayphil801@gmail.com>
2021-09-20 18:31:37 +0200

    removing custom commands

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-const customschema = require('../schemas/custom-commands');
+
 
 module.exports = (client, message) => {
     async function yup() {
@@ -9,8 +9,8 @@ module.exports = (client, message) => {
     const command = args.shift().toLowerCase();
     const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
     if (cmd) cmd.execute(client, message, args)
-    const data = await customschema.findOne({ Guild: message.guild.id, Command: command});
-    if(data) message.channel.send(data.Response)
+    
+    
 }   
     yup()
 };
