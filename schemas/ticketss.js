1f85c9dafddbe9eb5de4fd98c3b5b28a5ac9320c
philipp-spec <subwayphil801@gmail.com>
2021-12-21 09:41:46 +0100

    remove-tickets

--- schemas/ticketss.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-let Schema = new mongoose.Schema({
-      ticketcreator: String,
-      content: Array
-})
-
-module.exports = mongoose.model('tickets', Schema)
